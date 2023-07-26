4caf5715a47712a73ec46b541ba33a17efd95020
Juan S. López <dir.desarrollo@whitecloud.com.co>
2023-07-26 12:15:50 -0500

    first commit

---
+++ lib/presentation/screens/main_screen.dart
@@ -0,0 +1,63 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: FadeIn(
+          duration: const Duration(milliseconds: 1100),
+          child: const Text('Animate_do'),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(FontAwesomeIcons.twitter),
+            onPressed: () {},
+          ),
+          SlideInLeft(
+            from: 50,
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => const MainScreen()));
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: ElasticInRight(
+        child: FloatingActionButton(
+          child: const FaIcon(FontAwesomeIcons.play),
+          onPressed: () {},
+        ),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElasticIn(
+            duration: const Duration(milliseconds: 1300),
+            child: const Icon(Icons.new_releases, color: Colors.blue, size: 40),
+          ),
+          FadeInDown(
+            delay: const Duration(milliseconds: 200),
+            // duration: const Duration(milliseconds: 3000),
+            child: const Text('Titulo', style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200)),
+          ),
+          FadeInDown(
+            delay: const Duration(milliseconds: 800),
+            child: const Text('soy un texto pequeño', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
+          ),
+          FadeInLeft(
+            delay: const Duration(milliseconds: 1100),
+            child: Container(width: 220, height: 2, color: Colors.blue),
+          ),
+        ],
+      )),
+    );
+  }
+}
